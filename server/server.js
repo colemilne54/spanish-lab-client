c2ddd78713fbd3abb650185dcb8ddb555d9b9d91
Cole Milne <colemilne54@gmail.com>
2023-04-25 15:17:14 -0600

    changed code order

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const http = require('http').createServer(app);
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 
+// Enable CORS for all routes
+app.use(cors());
+
+app.use(express.json());
+app.use(express.static('public'));
+
 const options = {
     cors: {
         origin: 'https://spanish-lab-client.vercel.app',
@@ -12,12 +18,6 @@ const options = {
     }
 };
 const io = require('socket.io')(http, options);
-
-
-// Enable CORS for all routes
-app.use(cors());
-app.use(express.json());
-app.use(express.static('public'));
 
 app.use('/openai', require('./routes/openaiRoutes'));
 
